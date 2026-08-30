655e25aa5ac5d7c07eb6e5d41bde8166db19d800
messi003322-png <3.12707693e+08+messi003322-png@users.noreply.github.com>
2026-08-30 21:37:20 +0000

    Replace publisher script with Adcash autotag

--- scripts/advertising-scripts.js
+++ scripts/advertising-scripts.js
@@ -10,11 +10,12 @@ const fs = require('fs');
 const path = require('path');
 
 const DIST = path.join(__dirname, '..', 'dist');
-const SCRIPT_ID = 'publisher-script-a6cc452ba86aee46ecc8449d28cc7873';
-const SCRIPT_HTML = `<script id="${SCRIPT_ID}" type="text/javascript" data-cfasync="false">
-/*<![CDATA[/* */
-(function(){var c=window,t="a6cc452ba86aee46ecc8449d28cc7873",h=[["siteId",869*777-469*513+4883783],["minBid",0],["popundersPerIP","0"],["delayBetween",0],["default",false],["defaultPerDay",0],["topmostLayer","auto"]],r=["d3d3LnByZW1pdW12ZXJ0aXNpbmcuY29tL1ZYbWF6L2ljYW52YXMtbmVzdC5qcw==","ZDJqMDQyY2oxNDQxd2kuY2xvdWRmcm9udC5uZXQvVlQvakZKTVcvY2RjLm1pbi5jc3M="],k=-1,u,y,i=function(){clearTimeout(y);k++;if(r[k]&&!(1813957796000<(new Date).getTime()&&1<k)){u=c.document.createElement("script");u.type="text/javascript";u.async=!0;var x=c.document.getElementsByTagName("script")[0];u.src="https://"+atob(r[k]);u.crossOrigin="anonymous";u.onerror=i;u.onload=function(){clearTimeout(y);c[t.slice(0,16)+t.slice(0,16)]||i()};y=setTimeout(i,5E3);x.parentNode.insertBefore(u,x)}};if(!c[t]){try{Object.freeze(c[t]=h)}catch(e){}i()}})();
-/*]]>/* */
+const SCRIPT_ID = 'adcash-autotag-zxsbhnljps';
+const LIBRARY_HTML = '<script id="aclib" type="text/javascript" src="//acscdn.com/script/aclib.js"></script>';
+const AUTOTAG_HTML = `<script id="${SCRIPT_ID}" type="text/javascript">
+    aclib.runAutoTag({
+        zoneId: 'zxsbhnljps',
+    });
 </script>`;
 
 function walk(directory) {
@@ -31,6 +32,7 @@ function removePreviousAdvertising(html) {
     'ad-zone-loader-11663431',
     'publisher-script-574324156ab63a291444509b3c987f74',
     'publisher-script-69c6dedc489be65e1b8d38477134d7a1',
+    'adcash-autotag-zxsbhnljps',
     SCRIPT_ID
   ];
 
@@ -43,8 +45,8 @@ function removePreviousAdvertising(html) {
 
   // Retira redes o cargadores de monetización anteriores incrustados en HTML histórico.
   return result
-    .replace(/\s*<script\b[^>]*\bsrc=["'][^"']*(?:al5sm\.com|monetag|impassabletroubledwistful|profitableratecpmnetwork|adsterra)[^"']*["'][^>]*>\s*<\/script>\s*/gi, '\n')
-    .replace(/\s*<script\b[^>]*>\s*[\s\S]*?a6cc452ba86aee46ecc8449d28cc7873[\s\S]*?<\/script>\s*/gi, '\n');
+    .replace(/\s*<script\b[^>]*\bsrc=["'][^"']*(?:acscdn\.com|al5sm\.com|monetag|impassabletroubledwistful|profitableratecpmnetwork|adsterra|popads)[^"']*["'][^>]*>\s*<\/script>\s*/gi, '\n')
+    .replace(/\s*<script\b[^>]*>\s*[\s\S]*?(?:a6cc452ba86aee46ecc8449d28cc7873|zxsbhnljps|aclib\.runAutoTag|popads)[\s\S]*?<\/script>\s*/gi, '\n');
 }
 
 let processed = 0;
@@ -52,14 +54,24 @@ for (const file of walk(DIST).filter((entry) => entry.endsWith('.html'))) {
   const original = fs.readFileSync(file, 'utf8');
   let html = removePreviousAdvertising(original);
 
-  if (/<\/body\s*>/i.test(html)) {
-    html = html.replace(/<\/body\s*>/i, `${SCRIPT_HTML}\n</body>`);
+  if (/<\/head\s*>/i.test(html)) {
+    html = html.replace(/<\/head\s*>/i, `${LIBRARY_HTML}\n</head>`);
+  } else if (/<head\b[^>]*>/i.test(html)) {
+    html = html.replace(/<head\b[^>]*>/i, `$&\n${LIBRARY_HTML}`);
+  } else if (/<body\b[^>]*>/i.test(html)) {
+    html = html.replace(/<body\b[^>]*>/i, `${LIBRARY_HTML}\n$&`);
   } else {
-    html += `\n${SCRIPT_HTML}\n`;
+    html = `${LIBRARY_HTML}\n${html}`;
+  }
+
+  if (/<\/body\s*>/i.test(html)) {
+    html = html.replace(/<\/body\s*>/i, `${AUTOTAG_HTML}\n</body>`);
+  } else {
+    html += `\n${AUTOTAG_HTML}\n`;
   }
 
   fs.writeFileSync(file, html);
   processed += 1;
 }
 
-console.log(`[advertising-scripts] ${processed} HTML pages updated with one new publisher script; previous ad integrations removed.`);
+console.log(`[advertising-scripts] ${processed} HTML pages updated with Adcash library and autotag zone zxsbhnljps; previous ad integrations removed.`);
